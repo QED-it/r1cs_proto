d98197347bae3ba9512b8180467d56c2d8473fff
Kobi Gurkan <kobigurk@gmail.com>
2018-01-31 10:17:24 +0200

    Modifies to fit JSON marshalling

--- saver.go
+++ saver.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	r1cs "github.com/QED-it/r1cs_proto/gen"
+	jsonpb "github.com/golang/protobuf/jsonpb"
 	proto "github.com/golang/protobuf/proto"
 )
 
@@ -13,19 +14,19 @@ func main() {
 		Variables: []string{"ONE", "res", "A_0", "A_1", "A_2", "B_0", "B_1", "B_2", "compute_inner_product S_0"},
 		Constraints: []*r1cs.Constraint{
 			&r1cs.Constraint{
-				A: map[int64]int64{2: 1},
-				B: map[int64]int64{5: 1},
-				C: map[int64]int64{0: 0, 8: 1},
+				A: map[int32]int32{2: 1},
+				B: map[int32]int32{5: 1},
+				C: map[int32]int32{0: 0, 8: 1},
 			},
 			&r1cs.Constraint{
-				A: map[int64]int64{3: 1},
-				B: map[int64]int64{6: 1},
-				C: map[int64]int64{8: -1, 9: 1},
+				A: map[int32]int32{3: 1},
+				B: map[int32]int32{6: 1},
+				C: map[int32]int32{8: -1, 9: 1},
 			},
 			&r1cs.Constraint{
-				A: map[int64]int64{4: 1},
-				B: map[int64]int64{7: 1},
-				C: map[int64]int64{9: -1, 1: 1},
+				A: map[int32]int32{4: 1},
+				B: map[int32]int32{7: 1},
+				C: map[int32]int32{9: -1, 1: 1},
 			},
 		},
 	}
@@ -34,5 +35,11 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf(hex.EncodeToString(serialized))
+	fmt.Println(hex.EncodeToString(serialized))
+	jsonMarshaler := jsonpb.Marshaler{}
+	serializedJson, err := jsonMarshaler.MarshalToString(&system)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(serializedJson)
 }
